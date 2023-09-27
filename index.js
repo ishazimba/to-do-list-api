135ad58b5b5112e40c3f747facb7dc39f77ba90c
ishazimba <isha.zimba@gmail.com>
2023-09-27 05:28:23 +0100

    port edited

--- index.js
+++ index.js
@@ -78,6 +78,8 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("server has started on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
