130d94c95c04a33a27846490cec314a472c8ad60
ishazimba <isha.zimba@gmail.com>
2023-09-27 16:40:34 +0100

    ssl temporary fix

--- db.js
+++ db.js
@@ -8,6 +8,7 @@ const pool = new Pool({
   host: process.env.HOST,
   port: process.env.PORT,
   database: process.env.DATABASE,
+  ssl: { rejectUnauthorized: false },
 });
 // Test the connection
 pool.query("SELECT * FROM todo", (err, result) => {
