81380eb6d02d1743fbd16ebd07825c6dcd23b5aa
ishazimba <isha.zimba@gmail.com>
2023-09-27 04:28:49 +0100

    database created

---
+++ db.js
@@ -0,0 +1,12 @@
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
+
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: DB_PORT,
+  database: DB_DATABASE,
+});
+
+module.exports = pool;
